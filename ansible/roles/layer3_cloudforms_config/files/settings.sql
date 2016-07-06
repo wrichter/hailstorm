f66f9c4c1880d0355c618a3f9ad0e8ff6d1dae74
Christian Jung <chris@jung-christian.de>
2016-07-06 15:39:40 +0200

    setting company name, NTP and roles correctly

--- ansible/roles/layer3_cloudforms_config/files/settings.sql
+++ ansible/roles/layer3_cloudforms_config/files/settings.sql
@@ -1,6 +1,16 @@
-# enable roles
+-- enable roles
 update settings_changes set value='--- automate,database_operations,ems_inventory,ems_metrics_collector,ems_metrics_coordinator,ems_metrics_processor,ems_operations,event,reporting,scheduler,smartproxy,smartstate,user_interface,web_services,websocket\n---\n' where key='/server/role';
 
-# enable NTP
-update settings_changes set value='---\n192.168.101.1\n' where key='/ntp/server';
+-- enable NTP
+update settings_changes set value='---\n- 192.168.101.1\n' where key='/ntp/server';
+
+-- set Company
+DO
+$do$
+BEGIN
+  IF NOT EXISTS (select key,value from settings_changes where key='/server/company') THEN
+    insert into settings_changes (key,value,created_at,updated_at,resource_type,resource_id) VALUES ('/server/company','--- Hailstom\n...\n',now(),now(),'MiqServer',(select id from miq_servers));
+  END IF;
+END
+$do$
 
