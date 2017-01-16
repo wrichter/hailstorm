e86318ad7996ab5d7934ca641533b74656562aa4
Wolfram Richter <wrichter@redhat.com>
2017-01-16 14:20:19 +0100

    ensure that rhosp9 rollout continues to work

--- ansible/filter_plugins/core.py
+++ ansible/filter_plugins/core.py
@@ -4,6 +4,8 @@ def filter_list(list, key, value):
 def greater_than(list, key, value):
     return filter(lambda t: t[key] > value, list)
 
+def flatten(list_of_lists):
+    return filter(lambda l: [item for sublist in l for item in sublist], list_of_lists)
 
 class FilterModule(object):
     def filters(self):
